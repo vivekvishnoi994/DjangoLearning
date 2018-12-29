f67b56bd45f6c52dd870f67145a48eb4794487e1
Vivek <vivekvishnoi994@gmail.com>
2018-12-30 01:47:19 +0530

    .

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -3,6 +3,7 @@ Definition of urls for BasicProject.
 """
 
 from django.conf.urls import include, url
+import first_app.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,4 +19,7 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', first_app.views.index, name='index'),
+    url(r'^home$', first_app.views.index, name='home'),
 ]
---
+++ BasicProject/first_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class first_appConfig(AppConfig):
+    name = 'first_app'
